1e5c99bc6b5c29b583d2ff3056f9ddb6191f434b
Luke Nelson <luke@nelson.zone>
2023-12-22 22:20:32 +0000

    Refactor out the top level await

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import { PromisePool } from "@supercharge/promise-pool";
 import { fetchPokemonSpeciesList, getPokemonByName } from "./fetch";
 
 // Get the initial list of pokemon species
-const { results } = await fetchPokemonSpeciesList(1).then((pokemon_species) =>
-  // Use a promise pool to limit the number of concurrent requests
-  PromisePool.withConcurrency(10)
-    .for(pokemon_species)
-    .process((species) => getPokemonByName(species.name))
-);
+const results = () =>
+  fetchPokemonSpeciesList(1)
+    .then((pokemon_species) =>
+      // Use a promise pool to limit the number of concurrent requests
+      PromisePool.withConcurrency(10)
+        .for(pokemon_species)
+        .process((species) => getPokemonByName(species.name))
+    )
+    .then(({ results, errors }) => results);
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
