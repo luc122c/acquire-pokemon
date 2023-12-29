4654d8887414ef3f156216f4a6f0f09d677b324e
Luke Nelson <luke@nelson.zone>
2023-12-29 14:05:36 +0000

    Simple search form and styling

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerText = "Loading...";
 
+setupClearFormButton();
+
+const params = new URLSearchParams(window.location.search);
+const search = params.get("search");
 // Get the initial list of pokemon species
 fetchPokemonSpeciesList(1)
   .then((pokemon_species) =>
@@ -18,6 +22,15 @@ fetchPokemonSpeciesList(1)
       .process((species) => getPokemonByName(species.name))
   )
   .then(({ results }) => {
+    if (search) {
+      // Filter names and ids by the search term
+      return results.filter(
+        (pokemon) =>
+          pokemon.name.includes(search) || pokemon.id.toString() == search
+      );
+    } else return results;
+  })
+  .then((results) => {
     const grid = document.createElement("div");
     grid.classList.add("card-grid");
 
@@ -29,7 +42,24 @@ fetchPokemonSpeciesList(1)
         grid.appendChild(card);
       });
 
+    if (!results.length) {
+      const noResults = document.createElement("p");
+      noResults.classList.add("no-results");
+      noResults.textContent = "No results found.";
+      grid.appendChild(noResults);
+    }
+
     // Clear the div
     app.innerHTML = "";
     app.appendChild(grid);
   });
+
+function setupClearFormButton() {
+  const clearButton =
+    document.querySelector<HTMLButtonElement>("#clear-search")!;
+  clearButton.addEventListener("click", () => {
+    const url = new URL(window.location.href);
+    url.searchParams.delete("search");
+    window.location.href = url.href;
+  });
+}
--- src/rendering.ts
+++ src/rendering.ts
@@ -3,19 +3,23 @@ import type { PokemonSpecies } from "pokenode-ts";
 export const pokemonCard = (pokemon: PokemonSpecies) => {
   const card = document.createElement("div");
   card.classList.add("pokemon-card");
+  card.tabIndex = 1;
 
   const header = document.createElement("div");
   header.classList.add("pokemon-card-header");
 
   const title = document.createElement("h2");
+  title.classList.add("pokemon-card-title");
   title.textContent = pokemon.name;
   header.appendChild(title);
 
   const id = document.createElement("span");
+  id.classList.add("pokemon-card-id");
   id.textContent = `#${pokemon.id.toString().padStart(3, "0")}`;
   header.appendChild(id);
 
   const description = document.createElement("p");
+  description.classList.add("pokemon-card-description");
   // Find the english text if it exists, otherwise use the first one.
   description.textContent =
     pokemon.flavor_text_entries.find(
